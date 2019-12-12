11bdc61f854b4c9c5a4b156d4b0d44e35e5f6e94
jjmuesesq <jjmuesesq@unal.edu.co>
2019-12-12 17:43:39 -0500

    tutorial configuracion plantillas

--- crm/views.py
+++ crm/views.py
@@ -1,15 +1,5 @@
 from django.shortcuts import render, HttpResponse
 
-html_base = """
-<h1> CRM Confival </h1>
-<ul>
-    <li><a href="/home/">Portada</a></li>
-    <li><a href="/about-me/">Acerca de</a></li>
-    <li><a href="/portafolio/">Portafolio</a></li>
-    <li><a href="/contacto/">Contacto</a></li>
-</ul>
-"""
-
 # Create your views here.
 
 def home(request):
@@ -24,21 +14,13 @@ def home(request):
     return render(request, "crm/home.html", )
 
 def about(request):
-    return HttpResponse(html_base + """      
-        <h2> Acerca de </h2>
-        <p> Sentencias y conciliaciones - inversión en activos </p>    
-    """) 
+    # return HttpResponse(html_base + """      
+   
+    # """) 
+    return render(request, "crm/about.html")
 
 def portafolio(request):
-    # html_response = "<h1> CRM Confival </h1>"
-    # for i in range(10):
-    #     html_response += "<h2> Portada </h2>"
-    return HttpResponse(html_base + """
-        <h2> Portafolio </h2>
-        <p> Algunos de sus trabajos </p>
-    """)
+    return render(request, "crm/portafolio.html")   
 
 def contacto(request):
-    return HttpResponse(html_base + """      
-  
-    """)
+    return render(request, "crm/contact.html")
